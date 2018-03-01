2bba5fb362f56936c7a087d8bce73720cc536a2c
igor-provotorov <garynya@yandex.ru>
2018-03-01 14:13:02 +0300

    advanced-zeros/feature: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
-module.exports = function getZerosCount(number, base) {
-  // your implementation
+module.exports = function getresultsCount(number, base) {
+
+  let result = number, baseSystem = base, power, k, count;
+
+  for (let i = 2; i <= base; i++) {
+      if (baseSystem % i === 0) {
+          power = 0;
+          while (baseSystem % i === 0) {
+              power++;
+              baseSystem = parseInt(baseSystem / i);
+          }
+          count = 0;
+          k = number;
+          do {
+              count += parseInt(k / i);
+              k = parseInt(k / i);
+          } while (k != 0);
+          result = Math.min(result, parseInt(count / power));
+      }
+  }
+  return result;
 }
